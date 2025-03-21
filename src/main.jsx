ec2759c46dfe740b5de8d0b7fdca2f5e0cfcc518
MX <Mohmmed66mazen@gmail.com>
2025-03-21 10:30:07 +0200

    add not fount page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NoteFount from './NoteFount.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route index path='/' element={<App/>}/>
+      <Route path='*' element={<NoteFount/>} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
